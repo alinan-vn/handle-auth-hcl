3c82da6a6baddcb6c360e37209e0bd9cb5034e54
Alejandro Linan <jandrolinan@gmail.com>
2021-02-10 13:22:02 -0600

    first commit

---
+++ src/main/java/org/hcl/handleauthentication/HandleAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package org.hcl.handleauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandleAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandleAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hcl/handleauthentication/HandleAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcl.handleauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandleAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
